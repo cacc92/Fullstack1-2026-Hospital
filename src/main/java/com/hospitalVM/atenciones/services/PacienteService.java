d5186f8d3993b9a0a0394c089054f651964aa1e8
Cesar Carrasco <ccarrascocarre@gmail.com>
2026-04-21 10:46:50 -0400

    Se agrega la interfaz de service

---
+++ src/main/java/com/hospitalVM/atenciones/services/PacienteService.java
@@ -0,0 +1,15 @@
+package com.hospitalVM.atenciones.services;
+
+import com.hospitalVM.atenciones.models.Paciente;
+
+import java.util.List;
+
+public interface PacienteService {
+    List<Paciente> findAll();
+    Paciente findById(Long id);
+    Paciente findByCorreo(String correo);
+    Paciente findByRut(String rut);
+    Paciente save(Paciente paciente);
+    void deleteById(Long id);
+    Paciente updateById(Long id, Paciente paciente);
+}
